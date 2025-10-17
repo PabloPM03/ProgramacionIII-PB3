c92b26403c02f8dc4a7c097a14e256d7d3cbcec0
PabloPM03 <pablo.pasmue@usal.es>
2025-10-17 18:55:55 +0200

    Realización del Ejercicio 3: Compilación y Ejecución utilizando los comandos javac y java

---
+++ Semana 1/Ejercicios Obligatorios/Ejercicio_3/src/Prueba.java
@@ -0,0 +1,6 @@
+public class Prueba {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Soy la clase Prueba y voy a imprimir un Hola Mundo!");
+        System.out.println("Hello, World!");
+    }
+}
