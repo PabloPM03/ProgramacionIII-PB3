88d7dd15c1be8f751aaf238cdc0af315c4c13351
PabloPM03 <pablo.pasmue@usal.es>
2025-10-22 00:17:44 +0200

    Realización del Ejercicio_5 de la Semana 2: Imprimir por pantalla nombre y apellido (introducidos por el usuario) aplicando formato

---
+++ Semana 2/Ejercicios de Extensión/Ejercicio_5/src/App.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Introduzca su nombre: ");
+        String nombre = scan.nextLine();
+        System.out.println("Introduzca su apellido: ");
+        String apellido = scan.nextLine();
+
+            //LINEA DE * DE ARRIBA
+        System.out.printf("\n*");
+        for (int j=0; j<8; j++) {System.out.printf("*");}
+        if (nombre.length() > 6) {
+            for(int i=0; i<(nombre.length() - 6); i++) {System.out.printf("*");}
+        }
+        System.out.printf("*");
+        for (int j=0; j<10; j++) {System.out.printf("*");}
+        if (nombre.length() > 8) {
+            for(int i=0; i<(apellido.length() - 8); i++) {System.out.printf("*");}
+        }
+        System.out.printf("*");
+
+            //LINEA DE LEYENDA
+        System.out.printf("\n* NOMBRE ");
+        if (nombre.length() > 6) {
+            for(int i=0; i<(nombre.length() - 6); i++) {System.out.printf(" ");}
+        }
+        System.out.printf("* APELLIDO ");
+        if (nombre.length() > 8) {
+            for(int i=0; i<(apellido.length() - 8); i++) {System.out.printf(" ");}
+        }
+        System.out.printf("*");
+
+            //LINEA DE * DEL MEDIO
+        System.out.printf("\n*");
+        for (int j=0; j<8; j++) {System.out.printf("*");}
+        if (nombre.length() > 6) {
+            for(int i=0; i<(nombre.length() - 6); i++) {System.out.printf("*");}
+        }
+        System.out.printf("*");
+        for (int j=0; j<10; j++) {System.out.printf("*");}
+        if (nombre.length() > 8) {
+            for(int i=0; i<(apellido.length() - 8); i++) {System.out.printf("*");}
+        }
+        System.out.printf("*");
+
+            //LINEA DE NOMBRE Y APELLIDO INTRODUCIDOS
+        System.out.printf("\n* %s ", nombre);
+        if (nombre.length() < 6) {
+            for(int i=0; i<(6 - nombre.length()); i++) {System.out.printf(" ");}
+        }
+        System.out.printf("* %s ", apellido);
+        if (nombre.length() < 8) {
+            for(int i=0; i<(8 - apellido.length()); i++) {System.out.printf(" ");}
+        }
+        System.out.printf("*");
+
+            //LINEA DE * DE ABAJO
+        System.out.printf("\n*");
+        for (int j=0; j<8; j++) {System.out.printf("*");}
+        if (nombre.length() > 6) {
+            for(int i=0; i<(nombre.length() - 6); i++) {System.out.printf("*");}
+        }
+        System.out.printf("*");
+        for (int j=0; j<10; j++) {System.out.printf("*");}
+        if (nombre.length() > 8) {
+            for(int i=0; i<(apellido.length() - 8); i++) {System.out.printf("*");}
+        }
+        System.out.printf("*");
+
+        scan.close();
+    }
+}
